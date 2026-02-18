700541a909bbe7001c54a913a18d81c93f271b07
PRANTO KUMER PAUL <prantopaul55555@gmail.com>
2026-02-18 16:33:31 +0600

    Initialize backend project with essential files and dependencies - express - dotenv - cors - nodemon

---
+++ README.md
@@ -0,0 +1,12 @@
+# Backend With Express-JS
+
+## Description
+
+A boilerplate code for initializing express-js backend code. It is the minimum code that will help your server up and running.
+
+## Features
+- express-js 
+- cors 
+- json & query params
+- auto reload with nodemon
+
---
+++ index.js
@@ -0,0 +1,28 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+dotenv.config();
+
+const port = process.env.PORT || 3001;
+const app = express();
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+app.get('/health-checkup', (req, res) => {
+  res.send('server is up and running!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
